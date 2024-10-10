6650ad08cbe74f65b691f646dddfeaaf0f6ebca7
Jay <95573258+jayy-patell@users.noreply.github.com>
2024-10-10 11:51:09 +0530

    added navigation demo

---
+++ iosLab/Navigation_demo/Navigation_demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation_demo
+//
+//  Created by Student on 10/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
